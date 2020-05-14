0d407449210b9250add831f5f8a52763ea066a0c
Meredith Strickland <mere.strickland@gmail.com>
2020-05-14 19:04:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function solution(str){
+  return str.split('').reverse().join('')
+  // let finalArr = []
+
+  // let strArr = str.split('')
+
+  // for(let i = strArr.length-1; i>=0; i--){
+  //   finalArr.push(strArr[i])
+  // }
+
+  // return finalArr.join('')
+
+}
+
+solution('hello')
